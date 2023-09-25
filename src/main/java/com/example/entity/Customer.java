955434a2fa4c00a73d5ea701bc1f4d3b06756ee8
John Kilima <jkbundala@gmail.com>
2023-09-25 05:19:44 +0300

    A class without using lombok

---
+++ src/main/java/com/example/entity/Customer.java
@@ -0,0 +1,79 @@
+package com.example.entity;
+
+import java.util.Objects;
+
+public class Customer {
+	private Integer id;
+	private String name;
+	private String city;
+	private String email;
+	private String phone;
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", city=" + city + ", email=" + email + ", phone=" + phone
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, email, id, name, phone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		return Objects.equals(city, other.city) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(phone, other.phone);
+	}
+}
