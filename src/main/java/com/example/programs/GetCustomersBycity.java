cdfae206992606b15164bf7d7a9689eea88be559
John Kilima <jkbundala@gmail.com>
2023-10-01 18:58:38 +0300

    Get customers by city

---
+++ src/main/java/com/example/programs/GetCustomersBycity.java
@@ -0,0 +1,26 @@
+package com.example.programs;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.example.dao.CustomerDao;
+import com.example.dao.DaoFactory;
+import com.example.entity.Customer;
+
+public class GetCustomersBycity {
+
+	public static void main(String[] args) throws IOException {
+		CustomerDao dao = DaoFactory.getCustomerDao();
+		
+		String city = "Dodoma";
+
+		List<Customer> list = dao.getCustomerByCity(city);
+
+		System.out.println("There are " + list.size() + " customers from " + city);
+
+		for (Customer c : list) {
+			System.out.println(c);
+		}
+	}
+
+}
