ea2498329fb333c95e438ccd9b72fdc61b68552b
John Kilima <jkbundala@gmail.com>
2023-11-09 14:31:27 +0300

    Adding comment The true parameter indicates to automatically commit and close session to save data in db

--- src/main/java/com/example/dao/DaoFactory.java
+++ src/main/java/com/example/dao/DaoFactory.java
@@ -17,6 +17,7 @@ public final class DaoFactory {
 	public static CustomerDao getCustomerDao() throws IOException {
 		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+		// The true parameter indicates to automatically commit and close session to save data in db
 		SqlSession session = factory.openSession(true);
 		
 		return session.getMapper(CustomerDao.class);
